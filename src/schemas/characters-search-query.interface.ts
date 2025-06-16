2e1c867d335200c877ab3e8f4aed2ff9c90c101e
Alexander Skalatskiy <alexander.skalatskiy@riskiq.net>
2025-06-16 18:43:22 +0200

    feature: search characters - Created new service function and interface for characters searching

---
+++ src/schemas/characters-search-query.interface.ts
@@ -0,0 +1,5 @@
+export interface CharacterSearchQuery {
+    name?: string;
+    minBaseKi?: number;
+    maxBaseKi?: number;
+}
--- src/services/characters.service.ts
+++ src/services/characters.service.ts
@@ -1,17 +1,32 @@
 import type { Character } from "@/schemas/character.interface";
 import api from "./api.service";
-import type { CharactersResponse } from "@/schemas/charactersResponse.interface";
+import type { CharactersResponse } from "@/schemas/characters-response.interface";
+import type { CharacterSearchQuery } from "@/schemas/characters-search-query.interface";
 
 const CHARACTERS_URL = '/characters';
+const ITEMS_PER_PAGE = 40;
 
 export async function getAllCharacters(): Promise<Character[]> {
     const params = {
-        limit: 40
+        limit: ITEMS_PER_PAGE
     };
 
     const response = await api.get<CharactersResponse>(CHARACTERS_URL, { params });
 
-    console.log('getAllCharacters - response: ', response);
-
     return response.data?.items;
+}
+
+export async function searchCharacters(searchQuery: CharacterSearchQuery): Promise<Character[]> {
+     const params = {
+        limit: ITEMS_PER_PAGE,
+        name: searchQuery.name
+    };
+
+    const response = await api.get<CharactersResponse | Character[]>(CHARACTERS_URL, { params });
+
+    const characters: Character[] = Array.isArray(response.data) ? response.data : response.data.items;
+
+    // TODO: As api does not have the option to filter by Ki, should be done manually here
+
+    return characters;
 }
